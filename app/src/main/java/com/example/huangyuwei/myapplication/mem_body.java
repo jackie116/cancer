3056effee89070e0f99c351437f20e097c230e21
jackie116 <jackie1wu41@gmail.com>
2017-10-29 11:26:14 +0800

    add image

--- app/src/main/java/com/example/huangyuwei/myapplication/mem_body.java
+++ app/src/main/java/com/example/huangyuwei/myapplication/mem_body.java
@@ -1,17 +1,29 @@
 package com.example.huangyuwei.myapplication;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import java.text.NumberFormat;
 
 
 public class mem_body extends Fragment {
 
 
+    EditText edtcm,edtkg;
+    TextView edtresult;
+    Button btncount;
+    private LinearLayout linear_tab;
 
     public mem_body() {
         // Required empty public constructor
@@ -28,6 +40,37 @@ public class mem_body extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        edtcm=(EditText)getView().findViewById(R.id.input_cm);
+        edtkg=(EditText)getView().findViewById(R.id.input_kg);
+        edtresult=(TextView)getView().findViewById(R.id.result) ;
+        btncount=(Button)getView().findViewById(R.id.count);
+        linear_tab = (LinearLayout)getView().findViewById(R.id.linear_tab_signup);
+
+        linear_tab.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                InputMethodManager imm = (InputMethodManager)v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(v.getWindowToken(),0);
+                return false;
+            }
+        });
+
+        btncount.setOnClickListener(new View.OnClickListener() {
+            float cm,m,kg,bmi;
+            @Override
+            public void onClick(View v) {
+                cm=Float.parseFloat(edtcm.getText().toString());
+                m=cm/100;
+                kg=Float.parseFloat(edtkg.getText().toString());
+                NumberFormat nf = NumberFormat.getInstance();
+                nf.setMaximumFractionDigits( 2 );
+
+                bmi=kg/(m*m);
+                edtresult.setText("BMI="+nf.format(bmi));
+
+            }
+        });
 
 
     }
