88fbe745e0dca4476aeca8f009b94b9e21b023b4
jackie116 <jackie1wu41@gmail.com>
2017-11-23 00:37:15 +0800

    push

---
+++ app/src/main/java/com/example/huangyuwei/myapplication/mem/mood_main.java
@@ -0,0 +1,15 @@
+package com.example.huangyuwei.myapplication.mem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.huangyuwei.myapplication.R;
+
+public class mood_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mood_main);
+    }
+}
