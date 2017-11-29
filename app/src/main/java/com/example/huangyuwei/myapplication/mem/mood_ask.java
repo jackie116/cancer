6c7ffb761844a35eacfc6d54e2b8746a2c764277
jackie116 <jackie1wu41@gmail.com>
2017-11-29 16:03:32 +0800

    add

--- app/src/main/java/com/example/huangyuwei/myapplication/mem/mood_ask.java
+++ app/src/main/java/com/example/huangyuwei/myapplication/mem/mood_ask.java
@@ -1,9 +1,13 @@
 package com.example.huangyuwei.myapplication.mem;
 
+import android.content.Intent;
 import android.icu.text.SimpleDateFormat;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.huangyuwei.myapplication.R;
 
@@ -11,20 +15,20 @@ import java.util.Date;
 
 public class mood_ask extends AppCompatActivity {
 
+    ImageButton btn_start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mood_ask);
-        //先行定義時間格式
 
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
-
-//取得現在時間
-
-        Date dt=new Date();
-
-//透過SimpleDateFormat的format方法將Date轉為字串
-
-        String dts=sdf.format(dt);
+        btn_start= (ImageButton) findViewById(R.id.btn_start);
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext() , mood_qa.class);
+                startActivity(intent);
+            }
+        });
     }
 }
